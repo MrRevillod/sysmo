8433c3ee789c360d85d489c1eb1d9d7fb5835fd8
MrRevillod <lrevillod2022@alu.uct.cl>
2025-11-21 21:54:03 -0300

    First commit

---
+++ src/env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+	readonly SMTP_HOST: string
+	readonly SMTP_PORT: string
+	readonly SMTP_USER: string
+	readonly SMTP_PASS: string
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv
+}
